c6ad71a56bad55afaad386f513eb2d098b9a1511
Abdelilah <abdouajjouqa@gmail.com>
2024-11-13 12:31:59 +0100

    creat functions checkcarte and addtowishlist

--- assets/js/wishlist.js
+++ assets/js/wishlist.js
@@ -7,6 +7,30 @@ const addAllToCarte = document.getElementById("add-all-to-cart");
 const addToCarte = document.querySelectorAll("#add-to-card");
 const removeFromWishlist = document.querySelectorAll("#remove-from-wishlist");
 
+// arrays
+let wishlist = JSON.parse(localStorage.getItem("wishlist")) || [] // || []: mean if the wishlist is umpty we get an array umpty []
+let getCarteFav = [];
+let carteFav = [];
+
+function checkCarte(product){
+    for(let info of wishlist){
+        if(item.id === product.id){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
+
+function addToWishlist(product){
+    if(!checkCarte(product)){ //that mean the products isn't in wishlist
+        wishlist.push(product);
+        localStorage.setItem("wishlist", JSON.stringify(wishlist))
+    }
+}
+
+
+
 // remove carte
 for(let i=0; i<carte.length; i++){
     removeFromWishlist[i].addEventListener("click", ()=>{

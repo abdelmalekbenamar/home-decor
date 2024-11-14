1734be5a8a9ee09263e337bd6dbc56063abc8e15
BouizmouneSalma <salmabouizmoun@gmail.com>
2024-11-14 22:07:54 +0100

    obtenir le produit a partir du api

--- assets/js/about.js
+++ assets/js/about.js
@@ -1,2 +1,23 @@
 const products = 4; 
 const Top_p = document.getElementById("Top_P"); 
+let allProducts = []; 
+async function getProducts() {
+    try {
+        
+        const response = await fetch("https://decor.codia-dev.com/products.json");
+        if (!response.ok) {
+            throw new Error(`Erreur HTTP : ${response.status}`);
+        }
+
+        const data = await response.json();
+        
+        
+        allProducts = data.products;
+
+        
+
+    } catch (error) {
+        
+        console.error("Erreur lors de la récupération des produits :", error);
+    }
+}
